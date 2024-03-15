2737deb88260c8137b1501708f6dae2b751b8e12
Raja Madupu <rajamadupu@outlook.com>
2024-03-15 12:52:35 -0400

    new stuff added for page_allocate

--- mlpt.c
+++ mlpt.c
@@ -34,8 +34,8 @@ if (LEVELS > 1)
         ptbr_copy = valid_check(page_table_entry, offset_bits);
         va = va >> vpn_seg_bits;
     }
-    address_pointer = address_pointer + offset_bits;
-    translated_address = *address_pointer;
+     ptbr_copy = ptbr_copy + offset_bits;
+     translated_address = ptbr_copy;
 }else {
     vpn_mask = (0x1 << vpn_seg_bits) - 1;
     vpn_seg = va & vpn_mask;
@@ -64,33 +64,68 @@ size_t* datapage_allocated_address;
 
 if (ptbr_copy == 0 && LEVELS == 1)
 {
-            //Potential idea --> set ptbr to certain area in allocated page table
-            //Also have to allocate memory for a 'data' page for some bs idk
-            //Have to account for the fact that the first call makes two allocations, and subsequent calls may only make allocations for one page of data
-            //Check for validation for whether page number supplied to page_allocate is not marked valid in page table
+    if (posix_memalign(&pagetable_allocated_address, sizeof(size_t) * num_of_entries, sizeof(size_t) * num_of_entries) != 0) 
+    {
+        perror("Error with initial page entry allocated");
+    }
+    if (posix_memalign(&datapage_allocated_address, sizeof(size_t) * num_of_entries, sizeof(size_t) * num_of_entries) != 0) 
+    {
+        perror("Error with initial data page allocated");
+    }
+    vpn_mask = (0x1 << vpn_seg_bits) - 1;
+    vpn_seg = va & vpn_mask;
+    *ptbr_pointer = *pagetable_allocated_address;
+    size_t address_for_data = *datapage_allocated_address;
+    size_t phys_page_number_for_data = address_for_data / (sizeof(size_t) * num_of_entries);
+    size_t page_table_entry_for_page = (phys_page_number_for_data << POBITS) | 1;
+    ptbr_pointer[vpn_seg] = page_table_entry_for_page;
+
+}else if (ptbr_copy == 0 && LEVELS > 1) 
+{
+    //Formula for allocating page tables for multi-level
+        /*
+         * Allocate new pagetable --> Find address of pagetable --> set address to previous page_table_entry
+         */
     if (posix_memalign(&pagetable_allocated_address, sizeof(size_t) * num_of_entries, sizeof(size_t) * num_of_entries) != 0) 
     {
         perror("Error with page entry allocated");
     }
-    if (posix_memalign(&datapage_allocated_address, sizeof(size_t) * num_of_entries, sizeof(size_t) * num_of_entries) != 0) 
-    {
-        perror("Error with data page allocated");
-    }
-
     *ptbr_pointer = *pagetable_allocated_address;
-    size_t address_for_data = *datapage_allocated_address;
-    size_t phys_page_number_for_data = address_for_data / (sizeof(size_t) * num_of_entries);
-    size_t page_table_entry_for_page_0 = (phys_page_number_for_data << POBITS) | 1;
-    ptbr_pointer[0] = page_table_entry_for_page_0;
 
-}else 
+    for (int i = 1; i < LEVELS; i += 1)
+    {
+        vpn_mask = (0x1 << vpn_seg_bits) - 1;
+        vpn_seg = va & vpn_mask;
+        ptbr_copy = (vpn_seg * sizeof(size_t)) + ptbr_copy;
+        address_pointer = (size_t *) ptbr_copy;
+        page_table_entry = *address_pointer;
+        ptbr_copy = valid_check(page_table_entry, offset_bits); 
+        va = va >> vpn_seg_bits;
+       
+        if (i != LEVELS) 
+        {
+                if (posix_memalign(&pagetable_allocated_address, sizeof(size_t) * num_of_entries, sizeof(size_t) * num_of_entries) != 0) 
+                {
+                    perror("Error with page entry allocated");
+                }
+
+                size_t address_for_next_level = *pagetable_allocated_address;
+                size_t phys_page_number_for_level = address_for_next_level / (sizeof(size_t) * num_of_entries);
+                size_t page_table_entry_for_next_level_page = (phys_page_number_for_level << POBITS) | 1;
+                ptbr_pointer[vpn_seg] = page_table_entry_for_next_level_page;
+        }
+        
+    }
+     ptbr_copy = ptbr_copy + offset_bits;
+     translated_address = ptbr_copy;
+
+} else 
 {
     va = va >> POBITS;
     size_t vpn_mask;
     size_t vpn_seg;    
     if (LEVELS > 1)
     {
-        //!Have to implement method here to allocate pagetable when neccessary and data table when neccessary
         for (int i = 0; i < LEVELS; i += 1)
         {
             vpn_mask = (0x1 << vpn_seg_bits) - 1;
@@ -99,14 +134,19 @@ if (ptbr_copy == 0 && LEVELS == 1)
             address_pointer = (size_t *) ptbr_copy;
             page_table_entry = *address_pointer;
             ptbr_copy = valid_check(page_table_entry, offset_bits); 
-                if (ptbr_copy == 0xFFFFFFFFFFFFFFFF) 
+                //add implementation here for when new pagetable is needed b/c va exceeds page table entries allocated??
+                if (ptbr_copy == 0xFFFFFFFFFFFFFFFF && vpn_seg > num_of_entries) 
                 {
-
-
+                    
                 }
             va = va >> vpn_seg_bits;
         }
-        address_pointer = address_pointer + offset_bits;
+
+        if (ptbr_copy == 0xFFFFFFFFFFFFFFFF && vpn_seg <= num_of_entries) 
+        {
+            
+        }
+    //    address_pointer = address_pointer + offset_bits;
     //    translated_address = *address_pointer;
     }else {
         vpn_mask = (0x1 << vpn_seg_bits) - 1;
@@ -115,18 +155,17 @@ if (ptbr_copy == 0 && LEVELS == 1)
         address_pointer = (size_t *) ptbr_copy;
         page_table_entry = *address_pointer;
         translated_address = valid_check(page_table_entry, offset_bits);
-
         if (translated_address == 0xFFFFFFFFFFFFFFFF) 
         {
             if (posix_memalign(&datapage_allocated_address, sizeof(size_t) * num_of_entries, sizeof(size_t) * num_of_entries) != 0) 
             {
-                perror("Error with data page allocated");
+                perror("Error with data page allocated for missing entry");
             }
-
-            ptbr_copy = (vpn_seg * sizeof(size_t)) + ptbr_copy;
-            address_pointer = (size_t *) ptbr_copy;
-
-        }
+            size_t address_for_data = *datapage_allocated_address;
+            size_t phys_page_number_for_data = address_for_data / (sizeof(size_t) * num_of_entries);
+            size_t page_table_entry_for_page_vpnseg = (phys_page_number_for_data << POBITS) | 1;
+            ptbr_pointer[vpn_seg] = page_table_entry_for_page_vpnseg;
+        } 
     }   
 }
 }
